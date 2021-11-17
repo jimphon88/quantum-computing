29c48ff0a1110ae66a3f0c0a5920d0caf5d734e8
HalfBakedApe <anapestares@outlook.com>
2021-11-16 21:45:48 -0500

    hiding incomplete files

---
+++ source/colortools.py
@@ -0,0 +1,8 @@
+"""
+This is for colors used in plotting 
+"""
+
+
+color_box = ['#1f77b4','#ff7f0e','#2ca02c','#d62728',
+             '#9467bd','#8c564b','#e377c2','#7f7f7f',
+             '#bcdb22','#17becf']
